77e77bce05143ff48c007c459421d5ad15f05051
Aakashkumar Bhagat <ab997@afsconnect1.njit.edu>
2017-11-19 01:20:04 -0500

    Set Color and names

--- index.php
+++ index.php
@@ -1,4 +1,11 @@
 <?php
+ echo"<center> <h1> <u> Active Record </u> </h1> </center>";  // Displays Active record in center in bold with underline 
+ echo "<body style='background-color:#B0C4DE'>"; // Sets background color 
+ echo  "<center> <b>Attn: Prof. Keith Willams </b> <br> </center> "; // Displays Prof. Keith Williams in center in bold 
+ echo "<center> <b>TA: Ikjyot Singh Gujral </b><br></center> "; // Displays Ikjyot... in center in bold
+ echo "<center> <b>TA: Brandon Major </b><br></center> "; // Displays Barndon Major in center in bold
+ $a= date('m/d/y',time()) ; // s variable which stores current date 
+ echo "<center><b> Date: $a </b></center>"; // displays date in center in bold 
 //turn on debugging messages
 ini_set('display_errors', 'On');
 error_reporting(E_ALL);
